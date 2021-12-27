0fc6007c15f2078765d929873f1bcdf339b8d570
colormotor <drand48@gmail.com>
2021-12-27 12:19:30 +0000

    plot func

--- p5ut.js
+++ p5ut.js
@@ -12,15 +12,32 @@
 
 'use strict';
 var p5 = require("p5");
+var mth = require("./mth.js");
 
 p5.prototype.polygon2 = function(P, closed=false){
   this.beginShape();
   for (const p of P)
     this.vertex(p[0], p[1]);
   if (closed)
-    this.endShape(p5.CLOSE);
+    this.endShape(this.CLOSE);
   else
     this.endShape();
 }
+
+p5.prototype.plot = function(rect, y){
+  this.beginShape();
+
+  const w = rect[1][0] - rect[0][0];
+  const h = rect[1][1] - rect[0][1];
+  y = mth.div(y, mth.max(y));
+  const x = mth.linspace(rect[0][0], rect[1][0], y.length);
+  this.vertex(x[0], rect[1][1]);
+  for (var i = 0; i < y.length; i++)
+    this.vertex(x[i], rect[1][1] - y[i]*h);
+  this.vertex(rect[1][0], rect[1][1]);
+  this.endShape(this.CLOSE);
+}
+
+
 
 module.exports = p5;
