33d756bc3df9f38a61a81f5a04e56c7c5921ee8f
colormotor <drand48@gmail.com>
2021-12-29 10:28:02 +0000

    dp simplification and cleanup contour fixes

--- geom.js
+++ geom.js
@@ -21,7 +21,7 @@ const geom = function() { };
 
 
 /// Chord lengths for each segment of a contour
-geom.chord_lengths = ( P, closed=0 ) => {
+geom.chord_lengths = (P, closed = 0) => {
   if (closed)
     P = P.concat([P[0]]); //[...P, ...[P[0]]];
   var D = mth.diff(P, 0);
@@ -29,14 +29,14 @@ geom.chord_lengths = ( P, closed=0 ) => {
 }
 
 /// Cumulative chord lengths
-geom.cum_chord_lengths = ( P, closed=0 ) => {
+geom.cum_chord_lengths = (P, closed = 0) => {
   if (P.length < 2)
     return [];
   var L = geom.chord_lengths(P, closed);
   return mth.cumsum([0.0].concat(L));
 }
 
-geom.chord_length = ( P, closed=0 ) => {
+geom.chord_length = (P, closed = 0) => {
   if (P.length < 2)
     return 0;
   var L = geom.chord_lengths(P, closed);
@@ -56,103 +56,103 @@ geom.point_line_distance = (p, a, b) => {
   return Math.abs(mth.det([mth.sub(b, a), mth.sub(a, p)])) / mth.distance(b, a);
 }
 
-geom.point_segment_distance = ( p,a,b) => {
-  const d    = mth.sub(b, a);
-  const u    = mth.clamp(mth.dot(mth.sub(p, a), d) / mth.dot(d, d), 0, 1);
+geom.point_segment_distance = (p, a, b) => {
+  const d = mth.sub(b, a);
+  const u = mth.clamp(mth.dot(mth.sub(p, a), d) / mth.dot(d, d), 0, 1);
   const proj = mth.add(a, mth.mul(u, d));
   return mth.distance(proj, p);
 }
 
 geom.project_on_line = (p, a, b) => {
-    let d = mth.sub(b, a)
-    let t = mth.dot(mth.sub(p, a), d) / mth.dot(d, d);
-    return mth.add(a, mth.mul(mth.sub(b, a), t));
+  let d = mth.sub(b, a)
+  let t = mth.dot(mth.sub(p, a), d) / mth.dot(d, d);
+  return mth.add(a, mth.mul(mth.sub(b, a), t));
 }
 
-geom.rot_2d = (theta, affine=true ) => {
-    if (affine)
-      var d = 3;
-    else
-      var d = 2;
+geom.rot_2d = (theta, affine = true) => {
+  if (affine)
+    var d = 3;
+  else
+    var d = 2;
 
-    m = mth.eye(d)
-    ct = Math.cos(theta)
-    st = Math.sin(theta)
-    m[0][0] = ct; m[0][1] = -st
-    m[1][0] = st; m[1][1] = ct
-    return m
+  m = mth.eye(d)
+  ct = Math.cos(theta)
+  st = Math.sin(theta)
+  m[0][0] = ct; m[0][1] = -st
+  m[1][0] = st; m[1][1] = ct
+  return m
 }
 
-geom.trans_2d = ( xy) => {
-    var m = mth.eye(3);
-    m[0][2] = xy[0]
-    m[1][2] = xy[1]
-    return m
+geom.trans_2d = (xy) => {
+  var m = mth.eye(3);
+  m[0][2] = xy[0]
+  m[1][2] = xy[1]
+  return m
 }
 
-geom.scaling_2d = ( xy, affine=true ) => {
-    if (affine)
-      var d = 3;
-    else
-      var d = 2;
+geom.scaling_2d = (xy, affine = true) => {
+  if (affine)
+    var d = 3;
+  else
+    var d = 2;
 
-    if (mth.is_number(xy))
-      xy = [xy, xy]
+  if (mth.is_number(xy))
+    xy = [xy, xy]
 
-    var m = mth.eye(d)
-    m[0][0] = xy[0]
-    m[1][1] = xy[1]
-    return m
+  var m = mth.eye(d)
+  m[0][0] = xy[0]
+  m[1][1] = xy[1]
+  return m
 }
 
-geom.shear_2d = (xy, affine=true) => {
-    if (affine)
-      var d = 3;
-    else
-      var d = 2;
+geom.shear_2d = (xy, affine = true) => {
+  if (affine)
+    var d = 3;
+  else
+    var d = 2;
 
-    var m = mth.eye(d)
-    m[0][1] = xy[0]
-    m[1][0] = xy[1]
-    return m
+  var m = mth.eye(d)
+  m[0][1] = xy[0]
+  m[1][0] = xy[1]
+  return m
 }
 
 const affine_transform = (mat, data) => {
-  if (mth.dim(data).length==1){
-    return mth.slice(mth.dot(mat, data.concat([1.0])), -1 );
+  if (mth.dim(data).length == 1) {
+    return mth.slice(mth.dot(mat, data.concat([1.0])), -1);
   }
-  if (!geom.is_compound(data)){
+  if (!geom.is_compound(data)) {
     var n = data.length;
     var res = mth.transpose(
-      mth.slice( mth.dot(mat, mth.vstack([mth.transpose(data), [mth.ones(n)]])), -1));
+      mth.slice(mth.dot(mat, mth.vstack([mth.transpose(data), [mth.ones(n)]])), -1));
     return res;
   }
   // compound
-  return data.map(P=>affine_transform(mat, P));
+  return data.map(P => affine_transform(mat, P));
 }
 geom.affine_transform = affine_transform;
 geom.affine = affine_transform;
 
-geom.bounding_box = (S, padding=0) => {
-    /// Axis ligned bounding box of one or more contours (any dimension)
-    // Returns [min,max] list'''
+geom.bounding_box = (S, padding = 0) => {
+  /// Axis ligned bounding box of one or more contours (any dimension)
+  // Returns [min,max] list'''
   if (!S.length)
-    return [[0,0], [0,0]];
+    return [[0, 0], [0, 0]];
 
   if (!geom.is_compound(S))
     S = [S];
 
-  var bmin = mth.min(S.map(V=>mth.min(V, 0)), 0);
-  var bmax = mth.max(S.map(V=>mth.max(V, 0)), 0);
+  var bmin = mth.min(S.map(V => mth.min(V, 0)), 0);
+  var bmax = mth.max(S.map(V => mth.max(V, 0)), 0);
   return [mth.sub(bmin, padding), mth.add(bmax, padding)]
 }
 
 geom.make_rect = (x, y, w, h) => {
-    return [[x, y], [x+w, y+h]];
+  return [[x, y], [x + w, y + h]];
 }
 
 geom.make_centered_rect = (p, size) => {
-  return geom.make_rect(p[0] - size[0]*0.5, p[1] - size[1]*0.5, size[0], size[1]);
+  return geom.make_rect(p[0] - size[0] * 0.5, p[1] - size[1] * 0.5, size[0], size[1]);
 }
 
 geom.pad_rect = (rect, pad) => {
@@ -169,26 +169,26 @@ geom.rect_r = (rect) => rect[1][0];
 geom.rect_t = (rect) => rect[0][1];
 geom.rect_b = (rect) => rect[1][1];
 
-geom.rect_corners = (rect, close=false) => {
+geom.rect_corners = (rect, close = false) => {
   var [w, h] = rect_size(rect);
   var P = [mth.add(rect[0], 0), mth.add(rect[0], [w, 0]),
-       mth.add(rect[1], 0), mth.add(rect[0] + [0, h])];
+  mth.add(rect[1], 0), mth.add(rect[0] + [0, h])];
   if (close)
     P.append(P[0]);
   return P
 }
 
 geom.random_point_in_rect = (rect) => {
-  var x = mth.uniform( rect[0][0], rect[1][0] );
-  var y = mth.uniform( rect[0][1], rect[1][1] );
+  var x = mth.uniform(rect[0][0], rect[1][0]);
+  var y = mth.uniform(rect[0][1], rect[1][1]);
   return [x, y];
 }
 
-geom.scale_rect = (rect, s, halign=0, valign=0) => {
+geom.scale_rect = (rect, s, halign = 0, valign = 0) => {
   if (mth.is_number(s))
     s = [s, s];
   var [sx, sy] = s;
-  var r = rect.map(p=>p.slice(0));
+  var r = rect.map(p => p.slice(0));
   var origin = geom.rect_center(rect);
   if (halign == -1)
     origin[0] = geom.rect_l(rect);
@@ -203,43 +203,43 @@ geom.scale_rect = (rect, s, halign=0, valign=0) => {
 }
 
 /// Fit src rect into dst rect, preserving aspect ratio of src, with optional padding
-geom.rect_in_rect = (src, dst, padding=0., axis=-1) => {
+geom.rect_in_rect = (src, dst, padding = 0., axis = -1) => {
   var dst = geom.pad_rect(dst, padding);
   var [dst_w, dst_h] = mth.sub(dst[1], dst[0]);
   var [src_w, src_h] = mth.sub(src[1], src[0]);
 
-  var ratiow = dst_w/src_w;
-  var ratioh = dst_h/src_h;
+  var ratiow = dst_w / src_w;
+  var ratioh = dst_h / src_h;
 
-  if (axis < 0){
+  if (axis < 0) {
     if (ratiow <= ratioh)
-        axis = 1;
+      axis = 1;
     else
-        axis = 0;
+      axis = 0;
   }
-  if (axis==1){  // fit vertically [==]
+  if (axis == 1) {  // fit vertically [==]
     var w = dst_w;
-    var h = src_h*ratiow;
+    var h = src_h * ratiow;
     var x = dst[0][0];
-    var y = dst[0][1] + dst_h*0.5 - h*0.5;
-  }else{        // fit horizontally [ || ]
-    var w = src_w*ratioh;
+    var y = dst[0][1] + dst_h * 0.5 - h * 0.5;
+  } else {        // fit horizontally [ || ]
+    var w = src_w * ratioh;
     var h = dst_h;
     var y = dst[0][1];
-    var x = dst[0][0] + dst_w*0.5 - w*0.5;
+    var x = dst[0][0] + dst_w * 0.5 - w * 0.5;
   }
   return geom.make_rect(x, y, w, h);
 }
 
 /// Return homogeneous transformation matrix that fits src rect into dst
-geom.rect_in_rect_transform = (src, dst, padding=0., axis=None) => {
+geom.rect_in_rect_transform = (src, dst, padding = 0., axis = None) => {
   var fitted = geom.rect_in_rect(src, dst, padding, axis);
   var cenp_src = geom.rect_center(src);
   var cenp_dst = geom.rect_center(fitted);
 
   var M = mth.eye(3);
   M = mth.dot(M,
-              geom.trans_2d(mth.sub(cenp_dst, cenp_src)));
+    geom.trans_2d(mth.sub(cenp_dst, cenp_src)));
   M = mth.dot(M, geom.trans_2d(cenp_src));
   M = mth.dot(M, geom.scaling_2d(mth.div(rect_size(fitted), rect_size(src))));
   M = mth.dot(M, geom.trans_2d(mth.neg(cenp_src)));
@@ -251,8 +251,8 @@ geom.rect_to_rect_transform = (src, dst) => {
   var [dw, dh] = geom.rect_size(dst)
 
   var m = geom.trans_2d([dst[0][0], dst[0][1]])
-  m = mth.dot(m, geom.scaling_2d([dw/sw,dh/sh]))
-  m = mth.dot(m, geom.trans_2d([-src[0][0],-src[0][1]]))
+  m = mth.dot(m, geom.scaling_2d([dw / sw, dh / sh]))
+  m = mth.dot(m, geom.trans_2d([-src[0][0], -src[0][1]]))
 
   return m
 }
@@ -269,14 +269,14 @@ geom.line_intersection = (s0, s1, eps = 1e-10) => {
 }
 
 
-geom.line_intersection_uv = ( a1, a2, b1, b2, aIsSegment=False, bIsSegment=False) => {
+geom.line_intersection_uv = (a1, a2, b1, b2, aIsSegment = False, bIsSegment = False) => {
   const EPS = 0.00001;
   var intersection = mth.zeros(2);
   var uv = mth.zeros(2)
 
-  const denom  = (b2[1]-b1[1]) * (a2[0]-a1[0]) - (b2[0]-b1[0]) * (a2[1]-a1[1]);
-  const numera = (b2[0]-b1[0]) * (a1[1]-b1[1]) - (b2[1]-b1[1]) * (a1[0]-b1[0]);
-  const numerb = (a2[0]-a1[0]) * (a1[1]-b1[1]) - (a2[1]-a1[1]) * (a1[0]-b1[0]);
+  const denom = (b2[1] - b1[1]) * (a2[0] - a1[0]) - (b2[0] - b1[0]) * (a2[1] - a1[1]);
+  const numera = (b2[0] - b1[0]) * (a1[1] - b1[1]) - (b2[1] - b1[1]) * (a1[0] - b1[0]);
+  const numerb = (a2[0] - a1[0]) * (a1[1] - b1[1]) - (a2[1] - a1[1]) * (a1[0] - b1[0]);
 
   if (Math.abs(denom) < EPS)
     return [false, intersection, uv];
@@ -288,51 +288,49 @@ geom.line_intersection_uv = ( a1, a2, b1, b2, aIsSegment=False, bIsSegment=False
   intersection[1] = a1[1] + uv[0] * (a2[1] - a1[1]);
 
   var isa = true;
-  if (aIsSegment && (uv[0]  < 0 || uv[0]  > 1))
+  if (aIsSegment && (uv[0] < 0 || uv[0] > 1))
     isa = false;
   var isb = true;
-  if (bIsSegment && (uv[1] < 0 || uv[1]  > 1))
+  if (bIsSegment && (uv[1] < 0 || uv[1] > 1))
     isb = false;
 
-  return [isa&&isb, intersection, uv];
+  return [isa && isb, intersection, uv];
 }
 
-geom.line_segment_intersection = ( a1, a2, b1, b2 ) => {
-  var [res, intersection, uv] = geom.line_intersection_uv(a1,a2,b1,b2,false,true);
-    return [res, intersection];
+geom.line_segment_intersection = (a1, a2, b1, b2) => {
+  var [res, intersection, uv] = geom.line_intersection_uv(a1, a2, b1, b2, false, true);
+  return [res, intersection];
 }
 
-geom.segment_line_intersection = ( a1, a2, b1, b2 ) => {
-    var [res, intersection, uv] = geom.line_intersection_uv(a1,a2,b1,b2,true,false);
-    return [res, intersection];
+geom.segment_line_intersection = (a1, a2, b1, b2) => {
+  var [res, intersection, uv] = geom.line_intersection_uv(a1, a2, b1, b2, true, false);
+  return [res, intersection];
 }
 
-geom.segment_intersection = ( a1, a2, b1, b2 )  => {
-    var [res, intersection, uv] = geom.line_intersection_uv(a1,a2,b1,b2,true,true);
-    return [res, intersection];
+geom.segment_intersection = (a1, a2, b1, b2) => {
+  var [res, intersection, uv] = geom.line_intersection_uv(a1, a2, b1, b2, true, true);
+  return [res, intersection];
 }
 
-geom.line_ray_intersection = ( a1, a2, b1, b2 )  => {
-    var [res, intersection, uv] = geom.line_intersection_uv(a1,a2,b1,b2,false,false);
-    return [res && uv[1] > 0, intersection];
+geom.line_ray_intersection = (a1, a2, b1, b2) => {
+  var [res, intersection, uv] = geom.line_intersection_uv(a1, a2, b1, b2, false, false);
+  return [res && uv[1] > 0, intersection];
 }
 
-geom.ray_line_intersection = ( a1, a2, b1, b2 )  => {
-    var [res, intersection, uv] = geom.line_intersection_uv(a1,a2,b1,b2,false,false);
-    return [res && uv[0] > 0, intersection];
+geom.ray_line_intersection = (a1, a2, b1, b2) => {
+  var [res, intersection, uv] = geom.line_intersection_uv(a1, a2, b1, b2, false, false);
+  return [res && uv[0] > 0, intersection];
 }
 
-geom.ray_intersection = ( a1, a2, b1, b2 )  => {
-    var [res, intersection, uv] = geom.line_intersection_uv(a1,a2,b1,b2,false,false);
-    return [res && uv[0] > 0 && uv[1] > 0, intersection];
+geom.ray_intersection = (a1, a2, b1, b2) => {
+  var [res, intersection, uv] = geom.line_intersection_uv(a1, a2, b1, b2, false, false);
+  return [res && uv[0] > 0 && uv[1] > 0, intersection];
 }
 
-geom.ray_segment_intersection = ( a1, a2, b1, b2 )  => {
-    var [res, intersection, uv] = geom.line_intersection_uv(a1,a2,b1,b2,false,true);
-    return [res && uv[0] > 0 && uv[1] > 0, intersection];
+geom.ray_segment_intersection = (a1, a2, b1, b2) => {
+  var [res, intersection, uv] = geom.line_intersection_uv(a1, a2, b1, b2, false, true);
+  return [res && uv[0] > 0 && uv[1] > 0, intersection];
 }
-
-
 
 
 geom.random_radial_polygon = (n, min_r, max_r, center = [0, 0]) => {
@@ -340,8 +338,68 @@ geom.random_radial_polygon = (n, min_r, max_r, center = [0, 0]) => {
   let start = mth.uniform(0, Math.PI * 2);
   let Theta = mth.randspace(start, start + Math.PI * 2, n + 1).slice(0, -1);
   var res = mth.transpose([mth.add(mth.mul(mth.cos(Theta), R), center[0]),
-                           mth.add(mth.mul(mth.sin(Theta), R), center[1])]);
+  mth.add(mth.mul(mth.sin(Theta), R), center[1])]);
   return res;
+}
+
+
+// Internal dp simplify
+const dp_simplify = (X, eps, distfn = geom.point_line_distance) => {
+  var stack = [];
+  const n = X.length;
+  stack.push([0, n - 1])
+  var flags = _.range(0, n).map(v => true)
+
+  while (stack.length) {
+    var [a, b] = stack.pop();
+    var dmax = 0.0;
+    var index = a;
+
+    for (var i = a + 1; i < b; i++) {
+      if (flags[i]) {
+        var d = distfn(X[i], X[a], X[b])
+        if (d > dmax) {
+          index = i;
+          dmax = d
+        }
+      }
+    }
+
+    if (dmax > eps) {
+      stack.push([a, index])
+      stack.push([index, b])
+    } else {
+      for (var i = a + 1; i < b; i++)
+        flags[i] = false
+    }
+  }
+  return flags;
+}
+
+/**
+ * Ramer-Douglas-Peucker polyline simplification (sorry Rammer)
+ * @param {any} X
+ * @param {any} eps
+ * @param {bool} closed
+ * @returns simplified polyline
+ */
+geom.dp_simplify = (X, eps, closed = false, get_inds = false, distfn = geom.point_line_distance) => {
+  if (eps <= 0.) return ctr;
+
+  if (closed)
+    X = mth.vstack([X, [X[0]]]);
+
+  var flags = dp_simplify(X, eps, distfn);
+  var I = _.range(flags.length).filter(i => flags[i]);
+  var Y = I.map(i => X[i]);
+  if (closed) {
+    Y = Y.slice(0, Y.length - 1);
+    I = I.slice(0, I.length - 1);
+  }
+
+  if (get_inds)
+    return [Y, I];
+  return Y;
 }
 
 
@@ -353,76 +411,12 @@ geom.random_radial_polygon = (n, min_r, max_r, center = [0, 0]) => {
  * @param {bool} get_inds get cleaned up contour indices
  * @returns cleaned up contour and (optionally) indices
  */
+
 geom.cleanup_contour = (X, closed = false, eps = 1e-10, get_inds = false) => {
-  if (closed)
-    X = X.concat([X[0]]);
-  var D = mth.diff(X, 0);
-  var inds = _.range(0, X.length);
-  // chord lengths
-  var s = D.map((d) => d[0] ** 2 + d[1] ** 2);
-
-  // Delete values in input with zero distance
-  var Y = [X[0]];
-  var inds = [0];
-
-  for (var i = 0; i < s.length; i++) {
-    if (s[i] <= eps)
-      continue;
-    Y.push(X[i + 1]);
-    inds.push(i + 1);
-  }
-  if (closed) {
-    Y = X.slice(0, Y.length - 1);
-    inds = inds.slice(0, inds.length - 1);
-  }
-
-  if (get_inds)
-    return [Y, inds];
-  return Y;
+  const distfn = (p, a, b) => Math.max(mth.distance(p, a), mth.distance(p, b));
+  return geom.dp_simplify(X, eps, closed, get_inds, distfn);
 }
 
-const dp_simplify = (X, eps) => {
-  var dmax  = 0.0;
-  var   index = 0;
-  var   n     = X.length;
-  for (var i = 0; i < n; i++) {
-    const d = geom.point_line_distance(X[i], X[0], X[n - 1]);
-
-    if (d > dmax) {
-      index = i;
-      dmax  = d;
-    }
-  }
-
-  if (dmax >= eps) {
-    const x1 = dp_simplify(X.slice(0, index+1), eps);
-    const x2 = dp_simplify(X.slice(index, X.length), eps);
-    return mth.vstack([x1.slice(0, x1.length-1), x2]);
-  } else {
-    return [X[0], X[X.length-1]];
-  }
-}
-
-/**
- * Ramer-Douglas-Peucker polyline simplification (sorry Rammer)
- * @param {any} X
- * @param {any} eps
- * @param {bool} closed
- * @returns simplified polyline
- */
-geom.dp_simplify = (X, eps, closed=false)  => {
-  if (eps <= 0.) return ctr;
-
-  if (closed)
-    X = mth.vstack([X, [X[0]]]);
-
-  var Y = dp_simplify(X, eps);
-
-  if (closed) {
-    Y = Y.slice(0, Y.length-1);
-  }
-  return Y;
-}
 
 /**
  * schematize - C-oriented polyline schematization.
